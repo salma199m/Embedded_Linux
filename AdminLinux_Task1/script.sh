54511403dfc018b9ac232eb0e73cfefeda149848
salma <salmamahmoud.m99@gmail.com>
2023-12-01 18:33:57 +0200

    Task1 added

---
+++ AdminLinux_Task1/script.sh
@@ -0,0 +1,18 @@
+
+echo "hello"
+
+cd ~
+chomd +x script.h
+mkdir myDirectoy
+
+cd myDirectory
+
+mkdir secondDirectory
+
+touch myNotePaper
+
+cp myNotePaper myDirectory
+
+mv myNotePaper myOldNotePape
+
+
